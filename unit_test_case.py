64dbcc740eea30da0dc062d52f55291b14771f3b
Lux AEterna <luxaeternaluceateis@outlook.com>
2026-01-01 15:10:51 +0000

    修复Action的Bug

--- unit_test_case.py
+++ unit_test_case.py
@@ -7,7 +7,11 @@ class TC_Framework(TestCase):
     self.assertTrue(True, '123')
 
   def unit_002(self):
-    self.step('1. 单元测试，创建Action实例')
+    self.step('2. 单元测试，测试用例断言失败')
+    self.assertTrue(False, '123')
+
+  def unit_003(self):
+    self.step('3. 单元测试，创建Action实例')
     TestAction().action()
 
 
--- utest/core/case.py
+++ utest/core/case.py
@@ -2,25 +2,44 @@ from .base import Base
 
 
 class TestCase(Base):
-  """用例基类"""
+  """
+  测试用例基类
+  - 定义测试用例的行为
 
+  """
   @classmethod
-  def setUpClass(cls):
-    """初始化用例"""
+  def setUpClass(cls) -> None:
+    """
+    初始化测试用例
 
+    Returns:
+      return: 无
+
+    """
     # 实例化Base类，日志输出测试用例标题
     Base()._case(cls.__name__)
 
 
   @classmethod
-  def tearDownClass(cls):
-    """析构测试用例"""
+  def tearDownClass(cls) -> None:
+    """
+    析构测试用例
+
+    Returns:
+      return: 无
+
+    """
     pass
 
 
-  def setUp(self):
-    """初始化测试单元"""
+  def setUp(self) -> None:
+    """
+    初始化测试单元
 
+    Returns:
+      return: 无
+
+    """
     # 日志输出——测试单元标题
     self._unit(self.id().split('.')[-1])
 
@@ -28,9 +47,14 @@ class TestCase(Base):
     self._unit_passed = True
 
 
-  def tearDown(self):
-    """析构测试单元"""
+  def tearDown(self) -> None:
+    """
+    析构测试单元
 
+    Returns:
+      return: 无
+
+    """
     # 日志输出——测试单元是否通过
     if self._unit_passed:
       self._pass(f'{self._testMethodName} pass!')
@@ -38,11 +62,23 @@ class TestCase(Base):
       self._fail(f'{self._testMethodName} fail!')
 
 
-  def _formatMessage(self, msg, standard_msg):
-    """断言失败后将用例置为失败"""
+  def _formatMessage(self,
+    msg:         str | None,
+    standardMsg: str
+  ) -> str:
+    """
+    格式化错误信息
+    - 将断言失败的原始消息与自定义消息合并，生成最终的错误信息
 
-    super()._formatMessage(msg, standard_msg)
+    Args:
+      msg:         用户提供的自定义错误信息
+      standardMsg: 框架生成的默认错误信息
 
+    Returns:
+      return:      最终的错误信息
+
+    """
     # 将测试单元置为失败
     self._unit_passed = False
 
+    return super()._formatMessage(msg, standardMsg)
