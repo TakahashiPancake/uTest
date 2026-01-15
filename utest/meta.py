3ec889d38eac07cbf84f66eaa39e02b0ac2d9f1b
Lux AEterna <luxaeternaluceateis@outlook.com>
2026-01-15 15:03:13 +0000

    2026年01月15日修改

--- utest/meta.py
+++ utest/meta.py
@@ -4,15 +4,19 @@
 """
 from enum import StrEnum as _StrEnum
 
-
+# 配置文件夹
 CONFIG_DIR = 'config'
+
+# 日志文件夹
 LOG_DIR    = 'log'
 
+# 配置文件
 class CONFIGs(_StrEnum):
   AUTOPIP  = 'autopip.json'
   LOGGING  = 'logging.yaml'
   UNITTEST = 'unittest.yaml'
 
+# 日志文件
 class LOGs(_StrEnum):
   AUTOPIP = 'autopip.log'
 
