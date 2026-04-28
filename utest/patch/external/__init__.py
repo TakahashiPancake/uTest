21560f0923bfc29f85c66b7ecbf9cb9da7fff5f4
Pancake <yidong.zhu@hannto.com>
2026-04-28 21:22:49 +0800

    细分patch

---
+++ utest/patch/external/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['HtmlTestRunnerResultPatch', 'HtmlTestRunnerRunnerPatch']
+
+from utest.patch.external.HtmlTestRunner_patch import ResultPatch as HtmlTestRunnerResultPatch, RunnerPatch as HtmlTestRunnerRunnerPatch
+
