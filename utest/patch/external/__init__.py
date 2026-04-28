8b6d578523e96b8a1d35f588c493f18fec7f6a2e
TakahashiPancake <LuxAEterna@FoxMail.com>
2026-04-28 22:56:49 +0800

    重命名html-testrunner补丁

--- utest/patch/external/__init__.py
+++ utest/patch/external/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ['HtmlTestRunnerResultPatch', 'HtmlTestRunnerRunnerPatch']
 
-from utest.patch.external.HtmlTestRunner_patch import ResultPatch as HtmlTestRunnerResultPatch, RunnerPatch as HtmlTestRunnerRunnerPatch
+from utest.patch.external.html_testrunner_patch import ResultPatch as HtmlTestRunnerResultPatch, RunnerPatch as HtmlTestRunnerRunnerPatch
 
