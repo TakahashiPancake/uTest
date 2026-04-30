a26a382598ab83920ae34f9f820b7cfd1520010f
Pancake <yidong.zhu@hannto.com>
2026-04-30 17:48:58 +0800

    执行用例

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from utest import HTMLTestExecutor
-from cases import TC_Launch_MiJia
+from testcase.app.com_xiaomi_smarthome.app_test import *
 
 if __name__ == '__main__':
   html_executor = HTMLTestExecutor()
-  html_executor.load(TC_Launch_MiJia)
+  html_executor.load(TC_001_First_Time_Login, TC_002_Change_Locale)
   html_executor.run()
-
-
