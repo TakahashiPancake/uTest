07f6257d042180487c1635ff282712bc5b0f3617
Pancake <yidong.zhu@hannto.com>
2026-04-30 20:17:11 +0800

    修改示例case

--- example_case.py
+++ example_case.py
@@ -3,28 +3,28 @@ from example_action import test_action
 
 class TC_001_Framework(TestCase):
   def unit_001(self):
-    self.step('1. 单元测试，导入uTest包')
+    self.step('1. 测试断言成功')
     self.assertTrue(True, 'test_01')
 
   def unit_002(self):
-    self.step('2. 单元测试，测试用例断言失败')
+    self.step('2. 测试断言失败')
     self.assertTrue(False, 'test_02')
 
   def unit_003(self):
-    self.step('3. 单元测试，创建Action实例')
+    self.step('3. 调用action实例')
     test_action.action_03()
 
 
 class TC_002_Framework(TestCase):
   def unit_001(self):
-    self.step('1. 单元测试，导入uTest包')
+    self.step('1. 测试断言成功')
     self.assertTrue(True, 'test_01')
 
   def unit_002(self):
-    self.step('2. 单元测试，测试用例断言失败')
+    self.step('2. 测试断言失败')
     self.assertTrue(False, 'test_02')
 
   def unit_003(self):
-    self.step('3. 单元测试，创建Action实例')
+    self.step('3. 调用action实例')
     test_action.action_03()
 
