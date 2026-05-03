4a9370c0e03481d19bd92b7ef4639abf82c55d8e
TakahashiPancake <LuxAEterna@FoxMail.com>
2026-05-03 21:26:34 +0800

    修改示例测试用例

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from utest import HTMLTestExecutor
-from example_case import TC_001_Framework, TC_002_Framework, TC_003_Framework
+from example_case import \
+  TEST_CASE_EXAMPLE_001, \
+  TEST_CASE_EXAMPLE_002, \
+  TEST_CASE_EXAMPLE_003, \
+  TEST_CASE_EXAMPLE_004, \
+  TEST_CASE_EXAMPLE_005
 
 if __name__ == '__main__':
   executor = HTMLTestExecutor()
-  executor.load(TC_001_Framework, TC_002_Framework, TC_003_Framework)
+  executor.load(
+    TEST_CASE_EXAMPLE_001,
+    TEST_CASE_EXAMPLE_002,
+    TEST_CASE_EXAMPLE_003,
+    TEST_CASE_EXAMPLE_004,
+    TEST_CASE_EXAMPLE_005
+  )
   executor.run()
