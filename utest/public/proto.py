b309fc7d38f1b3f65acc6b204713a65f2593001a
Pancake <yidong.zhu@hannto.com>
2026-05-07 16:28:18 +0800

    在文件头部添加许可证摘要

--- utest/public/proto.py
+++ utest/public/proto.py
@@ -1,9 +1,9 @@
 """
 Copyright (c) 2026 Yidong Zhu
 
-Repository: https://github.com/TakahashiPancake/uTest
-
 Licensed under MIT (https://github.com/TakahashiPancake/uTest/blob/main/LICENSE)
+
+Repository: https://github.com/TakahashiPancake/uTest
 
 """
 from utest.util.proto import Proto as _Proto
